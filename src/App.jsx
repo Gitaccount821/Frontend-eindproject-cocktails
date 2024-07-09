fc591655a9d1fab0b4bb58047fb3affc54374705
Gitaccount821 <nealvanzomeren@gmail.com>
2024-07-09 21:59:23 +0200

    begin eindproject

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    console.log("App component loaded");
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>test test</p>
+        </div>
+    );
+};
+
+export default App;
+
