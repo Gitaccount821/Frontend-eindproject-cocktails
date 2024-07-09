3a899259cefb52ecf4f5608d4db43c04b9735c20
Gitaccount821 <nealvanzomeren@gmail.com>
2024-07-09 23:17:37 +0200

    eerste setup Headerbalksectie 1; vult nog niet hele scherm. maak later een constante

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import './App.css';
+import logoImage from './assets/cocktaillogoheader.png';
 
 const App = () => {
-    console.log("App component loaded");
     return (
-        <div>
-            <h1>Hello, World!</h1>
-            <p>test test</p>
+        <div className="app-container">
+            <main>
+                <section className="flex-container section1">
+                    <div className="logo-container">
+                        <img src={logoImage} alt="Logo" className="logo"/>
+                    </div>
+                    <div className="buttons-container">
+                        <button className="button">Aangeraden Cocktails</button>
+                        <button className="button">Favourieten Cocktails</button>
+                        <button className="button">Zoeken naar Cocktails</button>
+                        <button className="button">Contact</button>
+                        <button className="login">Login</button>
+                    </div>
+                </section>
+                <section className="flex-item">
+                    <h2>Section 2</h2>
+                    <p>Content for Section 2</p>
+                </section>
+                <section className="flex-item">
+                    <h2>Section 3</h2>
+                    <p>Content for Section 3</p>
+                </section>
+                <section className="flex-item">
+                    <h2>Section 4</h2>
+                    <p>Content for Section 4</p>
+                </section>
+            </main>
+            <footer>
+                <p>Footer content here</p>
+            </footer>
         </div>
     );
 };
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-console.log("Rendering App");
 
 ReactDOM.render(
     <React.StrictMode>
